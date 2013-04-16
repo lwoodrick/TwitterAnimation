a615a706bdc6ed724c6beff23b4f6ddc73c2f0bb
Lauren Woodrick <lauren@neo.com>
2013-04-16 10:13:10 -0400

    Loaded up files.
    Just like it says on the label.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GDI::Application.config.secret_token = 'de8f960c1cec22a89bcc34726c39ac9e4e37633d5db290fc6bad6a5c5300b903162a7f6964ab9e19e73e30d8247bcad753204f9e7013095971ecaacc434f06c1'
